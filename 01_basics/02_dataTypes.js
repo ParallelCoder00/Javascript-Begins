5d81e46c4178379728a8dff8d41c55071bcf83d4
Jatin Singh <jatinhappy.singh@gmail.com>
2025-11-27 16:05:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+("use strict") // treats all js code as a newer version
+
+// alert(3+3) // wrong, because we are using nodejs not browser
+
+console.log(3
+    +3) // This is just bad, it reduces readability drastically
+
+Datatypes
+//number = 2 to the power 53
+//bigint
+//string => ""
+//boolean => YES/NO
+//null = standalone value
+//undefined =>
+//symbol => unique
+//object
+//typeof
