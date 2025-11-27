ecdb560964804b78722ab6f8ccce4b585f5a094d
Jatin Singh <jatinhappy.singh@gmail.com>
2025-11-27 15:34:25 +0000

    Learned about var,const,let and their rules

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const AccountId = 2424242
+let accountEmail = "jatin@google.com"
+var accountpassword = "342354"
+accountcity = "Indore"
+
+// Never use Var , because it has scope issues
+
+console.table([AccountId, accountEmail, accountpassword, accountcity]) 
