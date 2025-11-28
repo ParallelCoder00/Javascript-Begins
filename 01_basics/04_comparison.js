e272bb71d1181180fc71838413346ac29136e577
Jatin Singh <jatinhappy.singh@gmail.com>
2025-11-28 04:38:45 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2 < 3)
+console.log(2 > 3)
+console.log(2 >= 3)
+console.log(2 <= 3)
+console.log(2 != 3)
+
+// These will give answers in true or false which is obvious
+
+console.log(null < 4)
+console.log(null > 4)
+console.log(null != 4)
+console.log(null <= 4)
+console.log(null >= 4)
+console.log(null == 4)
+
+// These are different the == operator works differently, the other operators will automatically convert the null into 0 to compare them so the answer will come in true and false, they dont give NaN output
+
+//These type of confusing code should not be written and avoid writing such code 
