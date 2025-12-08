b964bd1c69b868c476ad4187e4387215f4bcff85
JatinSingh <jatinhappy.singh@gmailcom>
2025-12-08 22:51:26 +0530

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,47 @@
+
+if(true){
+    let a = 2
+    const b = 3
+    var c = 4
+    // console.log(a);
+}
+let a = 200
+const b = 400
+
+
+// console.log(b);
+// console.log(c)
+
+function one(){
+    const username = "jatin"
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website)
+    // two()
+}
+one()
+
+if(true){
+    const username = "jatin"
+    if(true){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+function fun1(){
+    console.log("jatin");
+    
+}
+fun1()
+const store = function(){
+    console.log("nitin");
+}
+store()
+
+
