7bd9ec039cc36fad05e4911e8b57ab9252973caf
JatinSingh <jatinhappy.singh@gmailcom>
2025-12-08 23:34:39 +0530

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,23 @@
+// IIFE stand for - Immediately Invoked Function Expression
+
+// function chai(){
+//     console.log(`Jatin Is Back`);
+// }
+// chai()
+
+(function chai(){
+    //Named IIFE
+    console.log(`Jatin Is Back`);
+})();  // () => () works just like in arrow
+
+( ()  => {
+    // Simple IIFE
+    console.log(`Jatin Is Back 2`);
+})(); // Semicolon is imp in these both conditions to terminate the functoin immedietly
+
+
+
+
+
+
+
