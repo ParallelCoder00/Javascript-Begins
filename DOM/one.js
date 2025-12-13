23498dc9ca99499e761afacd3ffa0a05afcc9b4d
JatinSingh <jatinhappy.singh@gmailcom>
2025-12-13 12:59:55 +0530

    DOM edit remove and traversing

--- DOM/one.js
+++
@@ -1,5 +0,0 @@
-// DOM - Document Object Manupulation 
-// DOM has 4 pillars
-//1 Selecting elements of HTML and CSS
-//*querySelector , *querySelectorAll , *SelectByID , *SelectByClassName
-//2 Event Listner
