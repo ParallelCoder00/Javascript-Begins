0c2b0300069d6b844a8344ac6785c41ab6a9e7e6
JatinSingh <jatinhappy.singh@gmailcom>
2025-12-17 12:54:47 +0530

    promise

--- 09_Advance/Promise.js
+++ 09_Advance/Promise.js
@@ -80,3 +80,28 @@ async function sinkpromiseFour() {
   }
 }
 sinkpromiseFour();
+
+async function pullIn() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+pullIn();
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then((response) => {
+  return response.json()
+}).then((data) => {
+  console.log(data);
+  
+})
+.catch((error) => {
+  console.log(error);
+  
+})
